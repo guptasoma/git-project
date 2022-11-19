47d45ec9e6acb00e347a8cd80ab4203e7dba33a6
Soma <somag83@gmail.com>
2022-11-19 11:19:07 +0530

    Commit one

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+public class MainApp{
+
+		String message="Jenkins email notification" ;
+
+		public static void main(String[] args){
+			MainApp map=new MainApp();
+			
+			System.out.println("This is Java Jenkins Demo");
+
+			System.out.println("Message : "+map.message);
+		}
+
+}
