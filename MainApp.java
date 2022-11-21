40fbad05697c4c23fdf4736be239510a0d56e36d
Soma <somag83@gmail.com>
2022-11-21 10:08:55 +0530

    commit new changes to master

--- MainApp.java
+++ MainApp.java
@@ -10,6 +10,10 @@ public class MainApp{
 		
 			//Applied toUpperCase() method to the String value
 			System.out.println("Message : "+map.message.toUpperCase());
+			
+			//creating a Product instance
+			Product product=new Product("Samsung M32",13999.00);
+			System.out.println(product);
 		}
 
 }
